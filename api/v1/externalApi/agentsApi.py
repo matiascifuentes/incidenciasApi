c42b76961d8a0e9207c5de3a19ded1ae1c0b070a
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-13 19:40:05 -0300

    Creo la clase agentsApi.

---
+++ api/v1/externalApi/agentsApi.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+class AgentsApi:
+
+	def __init__(self):
+		self.endpoint = 'https://60280ddadd4afd001754aea8.mockapi.io/agents'
+
+	def create_agent(self,agent):
+		result = requests.post(self.endpoint, data = agent)
+		return result
