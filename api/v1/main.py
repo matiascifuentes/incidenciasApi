8b34df654946b865af3951e48490d153723c0dc1
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-13 19:40:27 -0300

    Agrego la ruta create_agent.

--- api/v1/main.py
+++ api/v1/main.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 import requests
 from externalApi.issuesApi import IssuesApi
+from externalApi.agentsApi import AgentsApi
 app = Flask(__name__)
 
-issuesDB = 'https://60280ddadd4afd001754aea8.mockapi.io/issues'
-agentsDB = 'https://60280ddadd4afd001754aea8.mockapi.io/agents'
+@app.route('/api/v1/agents', methods=['POST'])
+def create_agent():
+    if not request.json or not 'nombre' in request.json or not 'contrasena' in request.json:
+        abort(400)
+    agent = {
+        'nombre': request.json['nombre'],
+        'contrasena': request.json['contrasena']
+    }
+    agentsApi = AgentsApi()
+    result = agentsApi.create_agent(agent)
+    if result.status_code == 201:
+    	return jsonify({'agent': agent}), 201
+    return jsonify({'error': result.reason}), result.status_code
 
 @app.route('/api/v1/issues', methods=['GET'])
 def get_issues():
