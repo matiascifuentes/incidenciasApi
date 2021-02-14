b22321f0a97c5a45eeb0ece38c8f5e1f9e2d81fd
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-14 17:49:23 -0300

    Agrego ruta create_issue

--- api/v1/main.py
+++ api/v1/main.py
@@ -4,12 +4,13 @@ from flask import request
 import requests
 from externalApi.issuesApi import IssuesApi
 from externalApi.agentsApi import AgentsApi
+from externalApi.tokensApi import TokensApi
 app = Flask(__name__)
 
 @app.route('/api/v1/agents', methods=['POST'])
 def create_agent():
     if not request.json or not 'nombre' in request.json or not 'contrasena' in request.json:
-        abort(400)
+        return jsonify({'error': "Bad request"}), 400
     agent = {
         'nombre': request.json['nombre'],
         'contrasena': request.json['contrasena']
@@ -19,6 +20,26 @@ def create_agent():
     if result.status_code == 201:
     	return jsonify({'agent': agent}), 201
     return jsonify({'error': result.reason}), result.status_code
+
+@app.route('/api/v1/issue', methods=['POST'])
+def create_issue():
+    if not request.json or not 'fecha' in request.json or not 'titulo' in request.json or not 'descripcion' in request.json or not 'agente' in request.json or not 'token' in request.json:
+        return jsonify({'error': "Bad request"}), 400
+    issue = {
+        'fecha': request.json['fecha'],
+        'titulo': request.json['titulo'],
+        'descripcion': request.json['descripcion'],
+        'agente': request.json['agente']
+    }
+    tokensApi = TokensApi()
+    correctToken = tokensApi.verify_token(request.json['agente'], request.json['token'])
+    if correctToken:
+    	issueApi = IssuesApi()
+    	result = issueApi.create_issue(issue)
+    	if result.status_code == 201:
+	    	return jsonify({'issue': issue}), 201
+    	return jsonify({'error': result.reason}), result.status_code
+    return jsonify({'error': "Acceso denegado"}), 401
 
 @app.route('/api/v1/issues', methods=['GET'])
 def get_issues():
