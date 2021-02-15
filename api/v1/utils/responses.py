94bb9f53fe57c04254362b83ff72bc2785557564
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-15 20:57:15 -0300

    Actualizo funciones utilizando clase Error.

--- api/v1/utils/responses.py
+++ api/v1/utils/responses.py
@@ -1,32 +1,32 @@
 from flask import jsonify
-from utils.responseHeader import ResponseHeader
+from utils.error import Error
 
-def response(code, success, message, data = None):
-	header = ResponseHeader(code, success, message).info()
-	response = {
-		'status': header
-	}
-	if data:
-		response['data'] = data
+def response(success, code, reason = None, message = None, data = None):
+	response = {}
+	if success:
+		if data:
+			response = data
+	else:
+		response = Error(code, reason, message).info()
 	return jsonify(response), code, {'ContentType':'application/json'} 
 
 def http_200(data):
-	return response(200, True, "OK", data)
+	return response(True, 200, data = data)
 
-def http_201():
-	return response(201, True, "Created")
+def http_201(data):
+	return response(True, 201, data = data)
 
 def http_400():
-	return response(400, False, "Bad request")
+	return response(False, 400, "Bad request", "El cuerpo de la solicitud es incorrecto")
 
 def http_401():
-	return response(401, False, "Unauthorized")
+	return response(False, 401, "Unauthorized", "El token utilizado no es válido")
 
 def http_403():
-	return response(403, False, "Forbidden")
+	return response(False, 403, "Forbidden", "Usuario o clave incorrecto")
 
 def http_409():
-	return response(409, False, "Conflict. Already exists")
+	return response(False, 409, "Conflict", "El registro ya existe")
 
 def http_502():
-	return response(502, False, "Bad Gateway")
+	return response(False, 502, "Bad Gateway", "Ha ocurrido un error al conectar a la base de datos")
