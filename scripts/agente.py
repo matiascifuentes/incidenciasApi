6cdf5b5b2360feb64954be317f3eeb7ac785baf3
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-16 18:49:36 -0300

    Agrego control de error cuando API no está disponible

--- scripts/agente.py
+++ scripts/agente.py
@@ -4,7 +4,7 @@ from random import seed
 from random import randint
 seed(1)
 
-NUM_ISSUES = 10	#Cantidad de incidencias que se crearán
+NUM_ISSUES = 20	#Cantidad de incidencias que se crearán
 
 login_url = "http://localhost:8000/api/v1/agent/login"
 issue_url = "http://localhost:8000/api/v1/issue"
@@ -15,32 +15,35 @@ usuario = {
 }
 headers = {}
 
-response = requests.request("POST", login_url, headers=headers, json=usuario)
-if response.status_code == 201:
-	print("Login: OK")
-	datos = response.json()
-	agente = datos['agente']
-	token = datos['token']
+try:
+	response = requests.request("POST", login_url, headers=headers, json=usuario)
+	if response.status_code == 201:
+		print("Login: OK")
+		datos = response.json()
+		agente = datos['agente']
+		token = datos['token']
 
-	for i in range(0,NUM_ISSUES):
-		now = datetime.now()
-		fecha = now.strftime("%d-%m-%Y %H:%M:%S")
-		
-		datos = {
-			"fecha": fecha,
-			"titulo": "Incidencia " + str(randint(1,1000)),
-			"descripcion": "Descripcion",
-			"agente": usuario['nombre'],
-			"token": token
-		}
-		headers = {}
+		for i in range(0,NUM_ISSUES):
+			now = datetime.now()
+			fecha = now.strftime("%d-%m-%Y %H:%M:%S")
+			
+			datos = {
+				"fecha": fecha,
+				"titulo": "Incidencia " + str(randint(1,1000)),
+				"descripcion": "Descripcion",
+				"agente": usuario['nombre'],
+				"token": token
+			}
+			headers = {}
 
-		response = requests.request("POST", issue_url, headers=headers, json=datos)
-		if response.status_code == 201:
-			print("Issue " + str(i) + ": OK")
-		else:
-			print("Issue " + str(i) + ": FAILED")
-else:
-	print("Login: FAILED")
-	print(response.text)
+			response = requests.request("POST", issue_url, headers=headers, json=datos)
+			if response.status_code == 201:
+				print("Issue " + str(i) + ": OK")
+			else:
+				print("Issue " + str(i) + ": FAILED - " + str(response.status_code))
+	else:
+		print("Login: FAILED")
+		print(response.text)
+except:
+	print("Error al conectarse a la API")
 
