cad5753d3fdec3dda99395b416c23fe095aa9da2
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-16 12:54:54 -0300

    Agrego verificación de respuestas en create_agent y verify_agent

--- api/v1/externalApi/agentsApi.py
+++ api/v1/externalApi/agentsApi.py
@@ -7,22 +7,42 @@ class AgentsApi:
 		self.endpoint = 'https://60280ddadd4afd001754aea8.mockapi.io/agents'
 
 	def create_agent(self,agent):
-		result = requests.post(self.endpoint, data = agent)
-		success = False
-		if(result.status_code == 201):
-			success = True
-		return success
+		success, exists = self.agent_exists(agent['nombre'])
+		if success:
+			if not exists:
+				result = requests.post(self.endpoint, data = agent)
+				if(result.status_code == 201):
+					return True, 201
+				return False, 502
+			return False, 409
+		return False, 502
 
 	def get_all(self):
-		agents = requests.get(self.endpoint)
-		return agents.json()
+		result = requests.get(self.endpoint)
+		if result.status_code == 200:
+			return True, result.json()
+		return False, None
 
 	def verify_agent(self, agent):
-		agents = self.get_all()
-		i = 0
-		exits = False
-		while i < len(agents) and not exits:
-			if(agents[i]['nombre'] == agent['nombre'] and agents[i]['contrasena'] == agent['contrasena']):
-				exits = True
-			i = i + 1
-		return exits
+		success, agents = self.get_all()
+		if success:
+			i = 0
+			exists = False
+			while i < len(agents) and not exists:
+				if(agents[i]['nombre'] == agent['nombre'] and agents[i]['contrasena'] == agent['contrasena']):
+					exists = True
+				i = i + 1
+			return success, exists
+		return success, None
+
+	def agent_exists(self, usuario):
+		success, agents = self.get_all()
+		if success:
+			i = 0
+			exists = False
+			while i < len(agents) and not exists:
+				if(agents[i]['nombre'] == usuario):
+					exists = True
+				i = i + 1
+			return success, exists
+		return success, None
