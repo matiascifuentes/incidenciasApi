b8693444ce576ceb50a708c268823bda7ed8abb8
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2021-02-17 11:43:40 -0300

    Agrego función is_same_date

--- api/v1/utils/date_utils.py
+++ api/v1/utils/date_utils.py
@@ -6,3 +6,13 @@ def is_valid_date(dateString,format):
 		return True
 	except:
 		return False
+
+def is_same_date(dateString1,dateString2,format):
+	try:
+		date1 = datetime.strptime(dateString1, format)
+		date2 = datetime.strptime(dateString2, format)
+		if date1 == date2:
+			return True
+		return False
+	except:
+		return None
